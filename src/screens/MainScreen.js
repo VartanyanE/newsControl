3060994e1f4f4b9d33e1b48cddd799182545c0b8
Emanuil Vartanyan <emanuil_vartanyan@Emanuils-MBP.lan>
2021-08-19 23:00:45 -0700

    added styling

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { ScrollView, Text, StyleSheet, Button, FlatList, TextInput, TouchableOpacity } from "react-native";
+import { ScrollView, Text, StyleSheet, Button, FlatList, TextInput, TouchableOpacity , View} from "react-native";
 import { Card, ListItem, Icon } from "react-native-elements";
 import * as WebBrowser from "expo-web-browser";
 
@@ -26,6 +26,7 @@ const MainScreen = () => {
            
             setLink(response.data.articles[0].link)
              setFetchData(response.data.articles);
+             setText("");
           //  await console.log();
            
            
@@ -41,17 +42,20 @@ const MainScreen = () => {
     return (
       <ScrollView style={styles.main}>
         <TextInput
-          style={{ height: 40 }}
+          style={{ height: 60 , margin: 10}}
           placeholder="Whats your poison?"
           onChangeText={(text) => setText(text)}
           defaultValue={text}
         />
-        <Button onPress={fetchedData} title="News" />
+        <View style={styles.buttonMain}>
+        <Button onPress={fetchedData} title="Extract News" color="#841584"
+ />
+        </View>
         <FlatList
           data={fetchData}
           keyExtractor={({ id }, index) => id}
           renderItem={({ item }) => (
-            <Card>
+            <Card >
               <TouchableOpacity
                 onPress={handleOpenWithWebBrowser}
               ><Card.Title>{item.title}</Card.Title>
@@ -59,7 +63,6 @@ const MainScreen = () => {
               
               <Card.Divider />
               <Card.Title>{item.source.title}</Card.Title>
-              <Card.Divider />
             </Card>
           )}
         />
@@ -69,8 +72,18 @@ const MainScreen = () => {
 
 const styles = StyleSheet.create({
 
+  
+  
+
     main: {
-        margin: 10
+        backgroundColor: "#e0ebeb"
+    },
+    buttonMain: {
+        height: 60,
+        width: "100%",
+        color: "#000000"
+        
+        
     }
 });
 
